1bb8411929cc31619b2c11803c5866db70714f4f
Jörn Bungartz <joern@bungartz.pm>
2026-07-05 21:52:30 -0600

    feat: add n8nchat option defaults

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-    'name' => 'N8nChat'
+    'name' => 'N8nChat',
+
+    // Option defaults. Stored via \Option with the "n8nchat." prefix.
+    'options' => [
+        'enabled'           => ['default' => false],
+        'webhook_url'       => ['default' => ''],
+        'shared_secret'     => ['default' => ''],
+        'secret_header'     => ['default' => 'X-Freescout-Secret'],
+        'title'             => ['default' => ''],
+        'greeting'          => ['default' => ''],
+        'input_placeholder' => ['default' => ''],
+    ],
 ];
