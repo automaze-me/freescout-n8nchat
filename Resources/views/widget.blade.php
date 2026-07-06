36cc7979a93a695570f3504011871f9d3a5908ed
Jörn Bungartz <joern@bungartz.pm>
2026-07-06 12:50:34 -0600

    fix: markdown list indentation + bump widget text to 14px (v1.0.2)
    Scoped overrides.css (loaded after the vendored theme): restores clear bullet/numbered
    list indentation and raises message+input font to 14px (the widget's 1rem = ~10px on
    FreeScout's Bootstrap root).

--- Resources/views/widget.blade.php
+++ Resources/views/widget.blade.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="{{ asset($module_public.'/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset($module_public.'/css/overrides.css') }}">
 <script {!! \Helper::cspNonceAttr() !!}>window.N8nChatConfig = {!! json_encode($config, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT) !!};</script>
 <script type="module" src="{{ asset($module_public.'/js/loader.js') }}" {!! \Helper::cspNonceAttr() !!}></script>
